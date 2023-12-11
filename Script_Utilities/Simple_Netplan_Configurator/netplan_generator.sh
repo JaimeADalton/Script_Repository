7809a5699e374b2a38017f19d1c7a3273128781a
JaimeADalton <jaimeadalton@gmail.com>
2023-12-11 11:45:25 +0100

    Update netplan_generator.sh

--- Script_Utilities/Simple_Netplan_Configurator/netplan_generator.sh
+++ Script_Utilities/Simple_Netplan_Configurator/netplan_generator.sh
@@ -1,52 +1,65 @@
 #!/bin/bash
 
-# Crear archivo netplan
-echo "network:" > netplan.yaml
-echo "  version: 2" >> netplan.yaml
-echo "  renderer: networkd" >> netplan.yaml
+# Función para procesar una interfaz de red
+procesar_interfaz() {
+    local intf=$1
+    local ips=( $(ip addr show $intf | grep -o 'inet [0-9./]*' | awk '{print $2}') )
+    local routes=$(ip route | grep "$intf" | awk '{ if ($2 != "dev") { print "          - to: " $1 "\n            via: " $3 } }')
 
-# Procesar interfaces físicas
-echo "  ethernets:" >> netplan.yaml
-for intf in $(ls /sys/class/net/ | grep -vE 'vlan|lo'); do
-    ips=$(ip addr show $intf | grep -o 'inet [0-9./]*' | awk '{print $2}')
-    if [[ $ips ]]; then
-        echo "    $intf:" >> netplan.yaml
-        echo "      dhcp4: no" >> netplan.yaml
-        echo "      addresses:" >> netplan.yaml
-        for ip in $ips; do
-            echo "        - $ip" >> netplan.yaml
+    if [[ ${#ips[@]} -gt 0 ]]; then
+        echo "    $intf:"
+        echo "      dhcp4: no"
+        echo "      addresses:"
+        for ip in "${ips[@]}"; do
+            echo "        - $ip"
         done
 
-        # Verificar y agregar rutas para la interfaz
-        routes=$(ip route | grep "$intf" | awk '{ if ($2 != "dev") { print "          - to: " $1 "\n            via: " $3 } }')
         if [[ $routes ]]; then
-            echo "      routes:" >> netplan.yaml
-            echo "$routes" >> netplan.yaml
+            echo "      routes:"
+            echo "$routes"
         fi
     fi
-done
+}
 
-# Verificar si existen VLANs
-vlan_list=$(ip a s | grep @ | cut -d : -f2 | cut -d @ -f 1 | sed 's/\ //g')
-if [[ $vlan_list ]]; then
-    echo "  vlans:" >> netplan.yaml
+# Función para procesar una VLAN
+procesar_vlan() {
+    local vlan=$1
+    local id=$(echo $vlan | grep -o '[0-9]*')
+    local link=$(ip a s | grep $vlan@ | cut -d : -f2 | cut -d @ -f 2)
+    local ip=$(ip addr show $vlan | grep -o 'inet [0-9./]*' | awk '{print $2}')
+    local routes=$(ip route | grep "$vlan" | awk '{ if ($2 != "dev") { print "          - to: " $1 "\n            via: " $3 } }')
 
-    # Procesar VLANs
-    for vlan in $vlan_list; do
-        id=$(echo $vlan | grep -o '[0-9]*')
-        link=$(ip a s | grep $vlan@ | cut -d : -f2 | cut -d @ -f 2)
-        ip=$(ip addr show $vlan | grep -o 'inet [0-9./]*' | awk '{print $2}')
+    echo "    $vlan:"
+    echo "        id: $id"
+    echo "        link: $link"
+    echo "        addresses: [$ip]"
 
-        echo "      $vlan:" >> netplan.yaml
-        echo "          id: $id" >> netplan.yaml
-        echo "          link: $link" >> netplan.yaml
-        echo "          addresses: [$ip]" >> netplan.yaml
+    if [[ $routes ]]; then
+        echo "        routes:"
+        echo "$routes"
+    fi
+}
 
-        # Verificar y agregar rutas para la VLAN
-        routes=$(ip route | grep "$vlan" | awk '{ if ($2 != "dev") { print "          - to: " $1 "\n            via: " $3 } }')
-        if [[ $routes ]]; then
-            echo "          routes:" >> netplan.yaml
-            echo "$routes" >> netplan.yaml
-        fi
+# Crear archivo netplan
+{
+    echo "network:"
+    echo "  version: 2"
+    echo "  renderer: networkd"
+
+    # Procesar interfaces físicas
+    echo "  ethernets:"
+    for intf in $(ls /sys/class/net/ | grep -vE 'vlan|lo'); do
+        procesar_interfaz $intf
     done
-fi
+
+    # Verificar si existen VLANs
+    vlan_list=$(ls /sys/class/net/ | grep 'vlan')
+    if [[ $vlan_list ]]; then
+        echo "  vlans:"
+
+        # Procesar VLANs
+        for vlan in $vlan_list; do
+            procesar_vlan $vlan
+        done
+    fi
+} > netplan.yaml
