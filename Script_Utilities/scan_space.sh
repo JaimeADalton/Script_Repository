831757bbb536b54489170f7dbdd188aba0b3c7d8
JaimeADalton <jaimeadalton@gmail.com>
2024-01-10 12:24:08 +0100

    Update scan_space.sh

--- Script_Utilities/scan_space.sh
+++ Script_Utilities/scan_space.sh
@@ -17,17 +17,26 @@ directory="$1"
 
 # Verifica si la ruta del directorio es válida y no es un enlace simbólico
 if [ -d "$directory" ] && [ ! -L "$directory" ]; then
-    # Recorre el directorio especificado
-    for userdir in "$directory"/*; do
-        # Si el directorio existe y no es un enlace simbólico
-        if [ -d "$userdir" ] && [ ! -L "$userdir" ]; then
-            # Obtiene el nombre del usuario a partir del nombre del directorio
-            username=$(basename "$userdir")
-            # Usa du para calcular el tamaño del directorio del usuario
-            size=$(du -sh "$userdir" | cut -f1)
-            echo "$username: $size"
-        fi
-    done
+    # Obtiene la cantidad de subdirectorios en el directorio
+    num_subdirectories=$(find "$directory" -type d | wc -l)
+    
+    if [ "$num_subdirectories" -eq 1 ]; then
+        # Si solo hay un subdirectorio (el propio directorio), calcula el espacio de los archivos
+        total_size=$(du -sh "$directory" | cut -f1)
+        echo "Espacio usado por archivos en $directory: $total_size"
+    else
+        # Si hay más de un subdirectorio, muestra el espacio de cada uno
+        for userdir in "$directory"/*; do
+            # Si el directorio existe y no es un enlace simbólico
+            if [ -d "$userdir" ] && [ ! -L "$userdir" ]; then
+                # Obtiene el nombre del usuario a partir del nombre del directorio
+                username=$(basename "$userdir")
+                # Usa du para calcular el tamaño del directorio del usuario
+                size=$(du -sh "$userdir" | cut -f1)
+                echo "$username: $size"
+            fi
+        done
+    fi
 else
     echo "La ruta proporcionada no es un directorio válido o es un enlace simbólico."
 fi
