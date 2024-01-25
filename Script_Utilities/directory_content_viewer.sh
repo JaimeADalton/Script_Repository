bc50dc6dd5df4cd6891008044151a2155c45c7d8
JaimeADalton <jaimeadalton@gmail.com>
2024-01-25 19:13:18 +0100

    Update directory_content_viewer.sh

--- Script_Utilities/directory_content_viewer.sh
+++ Script_Utilities/directory_content_viewer.sh
@@ -8,11 +8,13 @@ RESET="\e[0m"
 # Función para mostrar la ayuda
 show_help() {
     echo "Uso: $0 [DIRECTORIO]..."
-    echo "  Explora todos los archivos en cada directorio especificado y muestra su contenido. Cada directorio debe ser un directorio válido."
+    echo "Explora todos los archivos en cada DIRECTORIO especificado y muestra su contenido."
+    echo "Cada DIRECTORIO debe ser un directorio válido."
 }
 
 # Comprueba si no se proporcionaron argumentos
 if [ "$#" -eq 0 ]; then
+    echo "Error: Se requiere al menos un argumento."
     show_help
     exit 1
 fi
@@ -28,7 +30,8 @@ for dir in "$@"; do
         for file in "$dir"/*; do
             # Comprueba si es un archivo regular
             if [ -f "$file" ]; then
-                echo -e "${YELLOW}Contenido de $file:${RESET}"
+                file_path=$(echo "$file" | sed 's/\/\//\//g')
+                echo -e "${YELLOW}Contenido de $file_path:${RESET}"
                 cat "$file" 2>/dev/null
                 echo "-------------------------"
             fi
