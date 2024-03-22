30a759c2aff94448fa25bfb83131d1f61211e132
JaimeADalton <jaimeadalton@gmail.com>
2024-03-22 08:55:25 +0100

    Update DockerInstall.sh

--- Deployment_Scripts/Docker/DockerInstall.sh
+++ Deployment_Scripts/Docker/DockerInstall.sh
@@ -3,21 +3,15 @@
 # Establece la variable DEBIAN_FRONTEND en "noninteractive"
 export DEBIAN_FRONTEND=noninteractive
 
-# Actualiza la lista de paquetes e instala las herramientas necesarias
-sudo apt-get update -q -y
-sudo apt-get install -q -y ca-certificates curl gnupg
-
-# Crea el directorio para el almacenamiento de claves
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Descarga y agrega la clave GPG de Docker al anillo de claves
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-# Otorga permisos de lectura a la clave GPG
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Agrega el repositorio de Docker al archivo sources.list.d
-echo "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Add the repository to Apt sources:
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Actualiza la lista de paquetes nuevamente antes de la instalación de Docker
 sudo apt-get update -q -y
