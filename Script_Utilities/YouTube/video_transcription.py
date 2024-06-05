3b3c67a16a65313eec71462a73faf7b2c1660055
JaimeADalton <jaimeadalton@gmail.com>
2024-06-05 14:57:46 +0200

    Update video_transcription.py

--- Script_Utilities/YouTube/video_transcription.py
+++ Script_Utilities/YouTube/video_transcription.py
@@ -1,3 +1,57 @@
+import os
+import re
+from googleapiclient.discovery import build
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound
+
+# Función para obtener detalles de los videos de un canal
+def get_video_details(api_key, channel_id, max_results=100):
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    video_details = []
+    next_page_token = None
+    results_retrieved = 0
+
+    while results_retrieved < max_results:
+        request = youtube.search().list(
+            part='id,snippet',
+            channelId=channel_id,
+            maxResults=min(max_results - results_retrieved, 50),  # Solicitar hasta 50 resultados a la vez
+            order='date',
+            type='video',  # Solo buscar videos subidos
+            pageToken=next_page_token
+        )
+        response = request.execute()
+        items = response.get('items', [])
+        results_retrieved += len(items)
+
+        for item in items:
+            if item['id']['kind'] == 'youtube#video':
+                video_id = item['id']['videoId']
+                title = item['snippet']['title']
+                # Limpiar el título para que se pueda usar como nombre de archivo
+                safe_title = re.sub(r'[\\/*?:"<>|]', "", title)
+                video_details.append({
+                    'url': f"https://www.youtube.com/watch?v={video_id}",
+                    'title': safe_title
+                })
+
+        next_page_token = response.get('nextPageToken')
+        if not next_page_token:
+            break
+
+    return video_details
+
+# Función para descargar la transcripción de un video
+def download_transcript(video_id, video_title, save_dir, lang='es'):
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=[lang])
+        transcription_text = ' '.join([segment['text'] for segment in transcript])
+
+        with open(os.path.join(save_dir, f"{video_title}_{lang}.txt"), 'w', encoding='utf-8') as file:
+            file.write(transcription_text)
+
+        print(f"Transcripción descargada para {video_title} en {lang}")
+    except (TranscriptsDisabled, NoTranscriptFound):
+        print(f"No hay transcripción para {video_title} en {lang}")
     except Exception as e:
         print(f"Ocurrió un error con el video {video_title}: {e}")
 
